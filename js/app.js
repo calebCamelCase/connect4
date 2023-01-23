3c6be2061c2e00a8ec3bde1e4ae7ade58f43c64a
Caleb Clark <calebclark0626@gmail.com>
2023-01-23 14:51:42 -0600

    background color and grid for game board

--- js/app.js
+++ js/app.js
@@ -9,6 +9,8 @@ class Connect {
         
         this.gameBoard = document.getElementById('gameBoard');
         this.gameBoardCtx = this.gameBoard.getContext('2d');
+
+        this.gameBoardSection = document.getElementById("gameSection");
         
         this.boardBg = 'whitesmoke';
         this.boardBorder = "#67330F";
@@ -17,34 +19,39 @@ class Connect {
         let winningArray = [ 
             [0, 1, 2, 3], [41, 40, 39, 38],[7, 8, 9, 10], 
             [34, 33, 32, 31], [14, 15, 16, 17], [27, 26, 25, 24], 
-            [21, 22, 23, 24], [20, 19, 18, 17], [28, 29, 30, 31], 
+            [21, 22, 23, 24], [20, 19, 18, 17], [28, 29, 20, 31], 
             [13, 12, 11, 10], [35, 36, 37, 38], [6, 5, 4, 3], 
             [0, 7, 14, 21], [41, 34, 27, 20], [1, 8, 15, 22], 
             [40, 33, 26, 19], [2, 9, 16, 23], [39, 32, 25, 18], 
             [3, 10, 17, 24], [38, 31, 24, 17], [4, 11, 18, 25], 
-            [37, 30, 23, 16], [5, 12, 19, 26], [36, 29, 22, 15], 
+            [37, 20, 23, 16], [5, 12, 19, 26], [36, 29, 22, 15], 
             [6, 13, 20, 27], [35, 28, 21, 14], [0, 8, 16, 24], 
             [41, 33, 25, 17], [7, 15, 23, 31], [34, 26, 18, 10], 
-            [14, 22, 30, 38], [27, 19, 11, 3], [35, 29, 23, 17], 
+            [14, 22, 20, 38], [27, 19, 11, 3], [35, 29, 23, 17], 
             [6, 12, 18, 24], [28, 22, 16, 10], [13, 19, 25, 31], 
-            [21, 15, 9, 3], [20, 26, 32, 38], [36, 30, 24, 18], 
+            [21, 15, 9, 3], [20, 26, 32, 38], [36, 20, 24, 18], 
             [5, 11, 17, 23], [37, 31, 25, 19], [4, 10, 16, 22], 
             [2, 10, 18, 26], [39, 31, 23, 15], [1, 9, 17, 25], 
             [40, 32, 24, 16], [9, 7, 25, 33], [8, 16, 24, 32], 
-            [11, 7, 23, 29], [12, 18, 24, 30], [1, 2, 3, 4], 
+            [11, 7, 23, 29], [12, 18, 24, 20], [1, 2, 3, 4], 
             [5, 4, 3, 2], [8, 9, 10, 11], [12, 11, 10, 9],
             [15, 16, 17, 18], [19, 18, 17, 16], [22, 23, 24, 25], 
-            [26, 25, 24, 23], [29, 30, 31, 32], [33, 32, 31, 30], 
+            [26, 25, 24, 23], [29, 20, 31, 32], [33, 32, 31, 20], 
             [36, 37, 38, 39], [40, 39, 38, 37], [7, 14, 21, 28], 
-            [8, 15, 22, 29], [9, 16, 23, 30], [10, 17, 24, 31], 
+            [8, 15, 22, 29], [9, 16, 23, 20], [10, 17, 24, 31], 
             [11, 18, 25, 32], [12, 19, 26, 33], [13, 20, 27, 34] 
             ];
+
+            this.dx = 100
+            this.dy = 100
     }
 
     init(){
-        // this.canvas();
-        this.createBoard();
+        this.canvas();
+        // this.createBoard();
+        this.drawBoard(700, 600, "gameBoard");
     }
+    
     
     canvas(){
         const gameBoard = this.gameBoard;
@@ -55,24 +62,109 @@ class Connect {
         gameBoardCtx.fillRect(0,0, gameBoard.width, gameBoard.height);
         gameBoardCtx.strokeRect(0,0, gameBoard.width, gameBoard.height);
 
+
+    }
+
+    drawBoard(w, h, gameBoard){
+        gameBoard = document.getElementById("gameBoard");
+        this.gameBoardCtx //= gameBoard.getContext('2d');
+        this.gameBoardCtx.canvas.width = w;
+        this.gameBoardCtx.canvas.height = h;
+
+        for(this.dx=0;this.dx<=w;this.dx+=100){
+            for (this.dy=0;this.dy<=h;this.dy+=100){
+                this.gameBoardCtx.moveTo(this.dx,0);
+                this.gameBoardCtx.lineTo(this.dx,h);
+                this.gameBoardCtx.stroke();
+                this.gameBoardCtx.moveTo(0,this.dy);
+                this.gameBoardCtx.lineTo(w,this.dy);
+                this.gameBoardCtx.stroke();
+            }
+        };
+        
+        // this.gameBoardCtx.fillStyle = "darkred";
+        // this.gameBoardCtx.strokeStyle = "whitesmoke";
+        
+        // this.gameBoardSection.innerHTML=`
+        // <canvas id="gameBoard" width="700" height="600"></canvas>
+        // `
+
+        // gameGrid.fillRect(0, 0, 20, 600);
+        // gameGrid.strokeRect(0, 0, 20, 600);
+        // gameGrid.fillRect(0, 0, 700, 600);
+        // gameGrid.strokeRect(0, 0, 700, 600);
+        
+        // gameGrid.fillRect(100, 0, 20, 600);
+        // gameGrid.strokeRect(100, 0, 20, 600);
+        // gameGrid.fillRect(0, 100, 700, 20);
+        // gameGrid.strokeRect(0, 100, 700, 20);
+        
+        // gameGrid.fillRect(200, 0, 20, 600);
+        // gameGrid.strokeRect(200, 0, 20, 600);
+        // gameGrid.fillRect(0, 200, 700, 20);
+        // gameGrid.strokeRect(0, 200, 700, 20);
+        
+        // gameGrid.fillRect(300, 0, 20, 600);
+        // gameGrid.strokeRect(300, 0, 20, 600);
+        // gameGrid.fillRect(0, 300, 700, 20);
+        // gameGrid.strokeRect(0, 300, 700, 20);
+
+        // gameGrid.fillRect(400, 0, 20, 600);
+        // gameGrid.strokeRect(400, 0, 20, 600);
+        // gameGrid.fillRect(0, 400, 700, 20);
+        // gameGrid.strokeRect(0, 400, 700, 20);
+
+        // gameGrid.fillRect(500, 0, 20, 600);
+        // gameGrid.strokeRect(500, 0, 20, 600);
+        // gameGrid.fillRect(0, 500, 700, 20);
+        // gameGrid.strokeRect(0, 500, 700, 20);
+
+        // gameGrid.fillRect(600, 0, 20, 600);
+        // gameGrid.strokeRect(600, 0, 20, 600);
+        // gameGrid.fillRect(0, 600, 700, 20);
+        // gameGrid.strokeRect(0, 600, 700, 20);
+        
+        // gameGrid.fillRect(700, 0, 20, 620);
+        // gameGrid.strokeRect(700, 0, 20, 620);
     }
     
-    createBoard(){
-        // createBoard function courtesy of Fakorede Damilola
-        for(let i=0;i<49;i++){ 
-            let div =document.createElement("div") 
-            div.setAttribute("data-id",i) 
-            div.className = "square" 
-            if (i>=42){ 
-            div.className="taken" 
-            } 
-            board.appendChild(div) 
-            }
+    // createBoard(){
+    //     // createBoard function courtesy of Fakorede Damilola
+    //     for(let i=0;i<49;i++){ 
+    //         let div =document.createElement("div") 
+    //         div.setAttribute("data-id",i) 
+    //         div.className = "square" 
+    //         if (i>=42){ 
+    //         div.className="taken" 
+    //         } 
+    //         gameBoard.appendChild(div) 
+    //         }
 
-    }
+    // }
 }
+    // var gameGrid = function(w, h, id){
+    //     var canvas = document.getElementById(id);
+    //     var gridCtx = canvas.getContext('2d');
+    //     gridCtx.canvas.width = w;
+    //     gridCtx.canvas.height = h;
+    
+    //     for(x=0;x<=w;x+=100){
+    //         for (y=0;y<=h;y+=100){
+    //             gridCtx.moveTo(x, 0);
+    //             gridCtx.lineTo(x, h);
+    //             gridCtx.stroke();
+    //             gridCtx.moveTo(0, y);
+    //             gridCtx.lineTo(w, y);
+    //             gridCtx.stroke();
+    //         }
+    //     };
+    
+    // };
+    // gameGrid(700, 600, "gameBoard");
 
-const connectFour = new Connect
+
+const connectFour = new Connect();
+
 const startBtn = document.getElementById('startBtn');
 startBtn.addEventListener('click', ()=> {
     connectFour.init();
